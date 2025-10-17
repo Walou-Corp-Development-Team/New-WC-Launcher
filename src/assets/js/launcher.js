e8ef4b0fbb3cf16c8532030da9ae489ef6fefb03
Walou41 <165910794+Walou41@users.noreply.github.com>
2025-10-17 11:21:47 +0200

    Patch RAM

--- src/assets/js/launcher.js
+++ src/assets/js/launcher.js
@@ -96,8 +96,8 @@ class Launcher {
                 java_config: {
                     java_path: null,
                     java_memory: {
-                        min: 8,
-                        max: 16
+                        min: 4,
+                        max: 12
                     }
                 },
                 game_config: {
